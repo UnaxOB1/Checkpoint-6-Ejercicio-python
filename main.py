1816adf971b32f92c5013fd65bd396fa39972b81
UnaxOB1 <152311868+UnaxOB1@users.noreply.github.com>
2024-08-15 11:14:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+class Usuario:
+  def __init__(self, nombre, contraseña):
+      self.nombre = nombre
+      self.contraseña = contraseña
+
+
+objeto = Usuario("David", "Naranja445")
+
+print(objeto)
